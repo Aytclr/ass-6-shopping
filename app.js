5e96735922f531dffb88227e9ee08d4927992a46
AYTC <aytacalir@gmail.com>
2024-08-18 00:10:43 +0300

    dom btn

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+const btn = document.querySelector("nav button");  // navbar içerisindeki sepetim butonu 
+
+const searchInput = document.querySelector(".form-control") // Search input
+
+const addBtn = document.querySelector(".addbtn"); // card içerisinde ki ekle btn
+
+const detailBtn = document.querySelector(".detailbtn"); // card içerisinde ki ekle btn
+
+
+
+
+
+//kontrol
+console.log(btn);
+console.log(searchInput);
+console.log(addBtn);
+console.log(detailBtn);
