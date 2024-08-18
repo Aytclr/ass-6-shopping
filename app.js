161c7e67adfd459f0aa148c9493fd880cce49ed5
AYTC <aytacalir@gmail.com>
2024-08-18 12:22:55 +0300

    ADDED MISSION PROJECT

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 
 
+//! CAPTURING DOM ELEMENTS
+
 const btn = document.querySelector("nav button");  // navbar içerisindeki sepetim butonu 
 
 const searchInput = document.querySelector(".form-control") // Search input
@@ -9,12 +11,54 @@ const addBtn = document.querySelector(".addbtn"); // card içerisinde ki ekle bt
 
 const detailBtn = document.querySelector(".detailbtn"); // card içerisinde ki ekle btn
 
+// console.log(btn);
+// console.log(searchInput);
+// console.log(addBtn);
+// console.log(detailBtn);
+
+
+//! PULLING FROM API
 
 
 
 
-//kontrol
-console.log(btn);
-console.log(searchInput);
-console.log(addBtn);
-console.log(detailBtn);
+
+
+//! SHOW PRODUCTS FROM API
+
+
+
+
+
+//! SETTING PRODUCT DESCRIPTION INFORMATION
+
+
+
+//! CATEGORY INFORMATION
+
+
+
+
+//! PRINTING SELECTED CATEGORY
+
+
+//! FILTER OF CATEGORY INFORMATION AND SEARCH OF PRODUCT NAME 
+
+
+//! ADD THE BASKED AND SHOW BASKET LINE 
+
+
+//! CALCULATING THE TOTAL PRICE OF THE PRODUCTS IN THE CARD 
+
+//! UPDATING THE PRODUCT QUANTITY IN THE CART
+
+//! STROING BASKET ARRAY IN LOCALSTORAGE
+
+
+
+//! **********INFORMATION 
+
+// ALL PRODUCT=> https://anthonyfs.pythonanywhere.com/api/products/
+// SINGLE PRODUCT => https://anthonyfs.pythonanywhere.com/api/products/{productID}
+
+// YOU CAN USE VITE , ADD MODULES 
