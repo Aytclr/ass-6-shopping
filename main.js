dba628bf57e5abc249e51284414bbe53dc31e9a1
AYTC <aytacalir@gmail.com>
2024-08-18 14:57:34 +0300

    add js

--- main.js
+++ main.js
@@ -1,1 +1,65 @@
 
+
+
+
+//! CAPTURING DOM ELEMENTS
+
+const btn = document.querySelector("nav button");  // navbar içerisindeki sepetim butonu 
+
+const searchInput = document.querySelector(".form-control") // Search input
+
+const addBtn = document.querySelector(".addbtn"); // card içerisinde ki ekle btn
+
+const detailBtn = document.querySelector(".detailbtn"); // card içerisinde ki ekle btn
+
+console.log(btn);
+console.log(searchInput);
+console.log(addBtn);
+console.log(detailBtn);
+
+
+//! PULLING FROM API
+
+
+
+
+
+
+//! SHOW PRODUCTS FROM API
+
+
+
+
+
+//! SETTING PRODUCT DESCRIPTION INFORMATION
+
+
+
+//! CATEGORY INFORMATION
+
+
+
+
+//! PRINTING SELECTED CATEGORY
+
+
+//! FILTER OF CATEGORY INFORMATION AND SEARCH OF PRODUCT NAME 
+
+
+//! ADD THE BASKED AND SHOW BASKET LINE 
+
+
+//! CALCULATING THE TOTAL PRICE OF THE PRODUCTS IN THE CARD 
+
+//! UPDATING THE PRODUCT QUANTITY IN THE CART
+
+//! STROING BASKET ARRAY IN LOCALSTORAGE
+
+
+
+//! **********INFORMATION 
+
+// ALL PRODUCT=> https://anthonyfs.pythonanywhere.com/api/products/
+// SINGLE PRODUCT => https://anthonyfs.pythonanywhere.com/api/products/{productID}
+
+// YOU CAN USE VITE , ADD MODULES 
